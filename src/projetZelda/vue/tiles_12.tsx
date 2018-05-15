0e2a5fc15b9dfeea98cb88a8e1db8012582f5f31
azeghoudi <39115647+azeghoudi@users.noreply.github.com>
2018-05-15 14:32:43 +0200

    Add files via upload

---
+++ src/projetZelda/vue/tiles_12.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_12" tilewidth="16" tileheight="16" margin="16" tilecount="312" columns="24">
+ <image source="tiles_12.png" width="400" height="224"/>
+</tileset>
